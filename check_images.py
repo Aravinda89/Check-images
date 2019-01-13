636c5bd99aee93f94e758d9ccf43ec7357fa37bd
Aravinda <31471559+Aravinda89@users.noreply.github.com>
2019-01-13 13:33:55 +0800

    Add files via upload
    Checking if images can be loaded without any error

---
+++ check_images.py
@@ -0,0 +1,14 @@
+def check_missing_files(images):
+    """
+    Check any file is missing
+    :param images:
+    :return:
+    """
+    for inx, img in enumerate(images):
+        try:
+            print(inx, '/', len(images))
+            fh = open(img, 'r')
+        except FileNotFoundError:
+            print("Can't find", img)
+            break
+    return
